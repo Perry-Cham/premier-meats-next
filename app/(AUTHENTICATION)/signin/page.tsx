3fc95f165305cead6c9219b703d8363b77d0f560
--global <--global>
2025-11-21 13:15:09 +0000

    Added .vscode to gitignore

--- app/(AUTHENTICATION)/signin/page.tsx
+++ app/(AUTHENTICATION)/signin/page.tsx
@@ -1,9 +1,16 @@
 import Sign_In_Card from "@/app/components/sign_in_card"
+interface credentials{
+  username:String,
+  password: String
+}
 function Login(){
+ async function sendData(data : credentials){
+    const response = await axios.post(`${process.env.NODE_URL}`)
+  }
   return(
   <main>
     <div className="w-full min-h-screen flex justify-center items-center">
-      <Sign_In_Card />
+      <Sign_In_Card sendData={sendData}/>
     </div>
   </main>)
 }
